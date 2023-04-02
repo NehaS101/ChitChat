b8c61d2a10336ea49208bb125757641cad688153
NehaS101 <solankineha297@gmail.com>
2023-04-02 23:40:24 +0530

    cloning done

---
+++ script/login.js
@@ -0,0 +1,5 @@
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click",() => {
+    window.location = "https://wild-gray-gorilla-garb.cyclic.app/auth/google";
+})
